761f5a6d8d6381d225052a29061f1f1f7d9e1d18
dong149 <donghoon149@gmail.com>
2022-02-25 00:17:45 +0900

    initial setting

---
+++ src/main/java/com/goozik/GoozikApiApplication.java
@@ -0,0 +1,13 @@
+package com.goozik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoozikApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoozikApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goozik/server/GoozikApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goozik.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoozikApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
